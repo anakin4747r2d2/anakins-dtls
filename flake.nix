ff061f61163b05b6ff18d99ecaa80efddf21565b
anakin4747r2d2 <274023134+anakin4747r2d2@users.noreply.github.com>
2026-04-27 23:54:54 +0000

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "anakins-dtls";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "anakins-dtls";
+
+          packages = with pkgs; [
+            bash
+          ];
+        };
+      });
+}
